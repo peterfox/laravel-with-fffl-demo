9acf029cdff78d43aecaafcc5c6d79b06f243438
Peter Fox <peter.fox@peterfox.me>
2023-01-06 01:03:23 +0000

    Configured

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -122,9 +122,11 @@
                         </div>
                     </div>
 
+                    @feature('laravel.version')
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </div>
+                    @endfeature
                 </div>
             </div>
         </div>
